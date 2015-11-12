b21ead914088a11a55404901ebffda69d4d9764d
davidzhu001 <qluoluo@icloud.com>
2015-11-12 14:25:38 -0600

    next chapter

--- src/app.js
+++ src/app.js
@@ -4,24 +4,25 @@ var express = require('express'),
 	  posts = require('./mock/posts.json');
 
 var app = express();
+
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/templates')
 
 app.get('/', function(req, res){
-	res.render('index.jade')
+	res.render('index')
 });
 
-app.get('/blog/:title?', function(req, res){
+app.get('/blog/:title?', function(req, res){ 
 	var title = req.params.title;
 	if (title === undefined) {
 		res.status(503);
-		res.send("This page is under construction!")
+		res.send("This page is under construction!");
 	} else {
 		var post = posts[title];
 		res.send(post);
 	}
 });
 
-app.listen(3000, function(){
-	console.log("The frontend server is running on port 3000!")
+app.listen(3000, function() {
+	console.log("The frontend server is running on port 3000!");
 });
